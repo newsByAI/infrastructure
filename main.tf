856bbf97e03500511df22c2ed354dc273729605f
zai <curaca492@gmail.com>
2025-09-05 18:21:12 -0500

    feat: adding terraform version

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 terraform {
+  required_version = "1.13.0"
   required_providers {
     google = {
       source  = "hashicorp/google"
       version = "7.1.1"
     }
   }
-}
+} 
 
 locals {
   environment = terraform.workspace
